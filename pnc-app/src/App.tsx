84f534c05475aa939b6cb7a44c8fcdbca977768c
SShakeeb <shakeebshirazi@gmail.com>
2022-06-01 20:11:39 +0300

    Basic Structure of React app

--- pnc-app/src/App.tsx
+++ pnc-app/src/App.tsx
@@ -1,67 +1,32 @@
-
 import React from "react";
+import './App.css';
 import {
-  Box,
-  Button,
-  CssBaseline,
-  Paper,
-  ThemeProvider,
-  Typography,
-  TextField,
+  Container
 } from "@mui/material";
-import { createTheme } from "@mui/material/styles";
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+import InputModal from './scenes/InputModal';
 function App() {
-    
-  // define theme
-  const theme = createTheme({
-    palette: {
-      primary: {
-        light: "#63b8ff",
-        main: "#0989e3",
-        dark: "#005db0",
-        contrastText: "#000",
-      },
-      secondary: {
-        main: "#4db6ac",
-        light: "#82e9de",
-        dark: "#00867d",
-        contrastText: "#000",
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box
-        height="100vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Paper
-          elevation={3}
-          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
-        >
-          <Typography color="primary.dark" variant="h1">
-            Check Prime Number
-          </Typography>
-          <Box
-            component="form"
-            sx={{
-              '& > :not(style)': { m: 1, width: '55ch' },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <TextField id="outlined-basic" label="Enter numbers seprated with ," variant="outlined" />
-            <Button variant="contained">Submit</Button>
-          </Box>
-        </Paper>
-      </Box>
-    </ThemeProvider>
+    <div>
+      <h1 className='App'>Check Prime Number</h1>
+      <Container><RoutingComponent /></Container>
+    </div>
+  );
+}
+
+function RoutingComponent() {
+  return(
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/scenes/InputModal' element={<InputModal />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ pnc-app/src/components/TextBox.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+const TextBox = () => {
+
+}
+
+export default TextBox;
