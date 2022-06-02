c4ae8662189a10ea379abdfccd13de6b5a9de91a
SShakeeb <shakeebshirazi@gmail.com>
2022-06-02 06:59:39 +0300

    PrimeCalServices with it's Unit Tests

---
+++ pnc-app/src/services/PrimeCalServices.test.tsx
@@ -0,0 +1,21 @@
+import { getTestSum, getTestPrimeCheck, getTestPrimeCheckFalse, Numbers_To_Sum, Number_Is_Prime, Number_Is_Not_Prime } from "../tests/Fixtures";
+import { getSum, getPrimeCheck } from "./PrimeCalServices";
+
+describe("PrimeCalServices", () => {
+    describe("getTestSum", () => {
+        it("should return sum of number with it's status of prime number", async () => {
+            const result = await getSum(Numbers_To_Sum);
+            expect(result.data).toEqual(getTestSum());
+        });
+    });
+    describe("getTestPrimeCheck", () => {
+        it("should return true if number is a prime number", async () => {
+            const result = await getPrimeCheck(Number_Is_Prime);
+            expect(result.data).toEqual(getTestPrimeCheck());
+        });
+        it("should return false if number is not a prime number", async () => {
+            const result = await getPrimeCheck(Number_Is_Not_Prime);
+            expect(result.data).toEqual(getTestPrimeCheckFalse());
+        });
+    });
+});
